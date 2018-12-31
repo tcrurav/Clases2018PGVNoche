edf1ec17cb3e1351d053bc455631df20f0774b0a
tcrurav <tiburcio.cruz@gmail.com>
2018-12-31 16:02:13 +0000

    con hilos para responder a jose juan funcionando...

--- 20181219/PSP05_CONT_R40_ServidorHTTP/ServerHTTP/src/PaquetePrincipal/ServidorHTTP.java
+++ 20181219/PSP05_CONT_R40_ServidorHTTP/ServerHTTP/src/PaquetePrincipal/ServidorHTTP.java
@@ -41,9 +41,9 @@ class ServidorHTTP {
       socCliente = socServidor.accept();
       //atiendo un cliente
       System.out.println("Atendiendo al cliente ");
-      procesaPeticion(socCliente);
+      new AtenderCliente(socCliente).start();
       //cierra la conexión entrante
-      socCliente.close();
+      //socCliente.close();
       System.out.println("cliente atendido");
     }
   }
@@ -54,66 +54,7 @@ class ServidorHTTP {
    *
    * @throws IOException
    */
-  private static void procesaPeticion(Socket socketCliente) throws IOException {
-    //variables locales
-    String peticion;
-    String html;
-
-    //Flujo de entrada
-    InputStreamReader inSR = new InputStreamReader(
-            socketCliente.getInputStream());
-    //espacio en memoria para la entrada de peticiones
-    BufferedReader bufLeer = new BufferedReader(inSR);
-
-    //objeto de java.io que entre otras características, permite escribir 
-    //'línea a línea' en un flujo de salida
-    PrintWriter printWriter = new PrintWriter(
-            socketCliente.getOutputStream(), true);
-
-    //mensaje petición cliente
-    peticion = bufLeer.readLine();
-
-    //para compactar la petición y facilitar así su análisis, suprimimos todos 
-    //los espacios en blanco que contenga
-    peticion = peticion.replaceAll(" ", "");
-
-    //si realmente se trata de una petición 'GET' (que es la única que vamos a
-    //implementar en nuestro Servidor)
-    if (peticion.startsWith("GET")) {
-      //extrae la subcadena entre 'GET' y 'HTTP/1.1'
-      peticion = peticion.substring(3, peticion.lastIndexOf("HTTP"));
-
-      //si corresponde a la página de inicio
-      if (peticion.length() == 0 || peticion.equals("/")) {
-        //sirve la página
-        html = Paginas.html_index;
-        printWriter.println(Mensajes.lineaInicial_OK);
-        printWriter.println(Paginas.primeraCabecera);
-        printWriter.println("Content-Length: " + html.length() + 1);
-        printWriter.println("\n");
-        printWriter.println(html);
-      } //si corresponde a la página del Quijote
-      else if (peticion.equals("/quijote")) {
-        //sirve la página
-        html = Paginas.html_quijote;
-        printWriter.println(Mensajes.lineaInicial_OK);
-        printWriter.println(Paginas.primeraCabecera);
-        printWriter.println("Content-Length: " + html.length() + 1);
-        printWriter.println("\n");
-        printWriter.println(html);
-      } //en cualquier otro caso
-      else {
-        //sirve la página
-        html = Paginas.html_noEncontrado;
-        printWriter.println(Mensajes.lineaInicial_NotFound);
-        printWriter.println(Paginas.primeraCabecera);
-        printWriter.println("Content-Length: " + html.length() + 1);
-        printWriter.println("\n");
-        printWriter.println(html);
-      }
-    
-    }
-  }
+  
 
   /**
    * **************************************************************************
